8bcb888951c72b4f834cbef157ed0cdb3b2ed28b
Daniel Borowski <danielborowski05@gmail.com>
2022-11-21 19:30:38 +0100

    Netlify fix v2

--- src/lib/Navigation.svelte
+++
@@ -1,7 +0,0 @@
-<ul
-	class="md:flex flex-col md:flex-row items-center gap-8 text-lg font-normal text-center mt-4 md:mt-0 shadow-lg rounded-lg w-full p-6 space-y-2 md:w-fit md:p-0 md:shadow-none md:rounded-none"
->
-	<li><a href="/">Strona główna</a></li>
-	<li><a href="/work">Usługi</a></li>
-	<li><a href="/contact">Kontakt</a></li>
-</ul>
